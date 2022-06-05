8ff4c31c9e867e85f2728f4bf0f7f03ec82c2815
Ajay <ajaysarwade339@gmail.com>
2022-06-05 10:55:01 +0530

    debugged

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -35,7 +35,7 @@ export class TodosComponent implements OnInit {
 
   add(todo:Todo)
   {
-    if(todo.title!=null && todo.desc!=null)
+    if(todo.title!="" && todo.desc!="")
     {
       this.todos.push(todo)
       localStorage.setItem("todos",JSON.stringify(this.todos))
